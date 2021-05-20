4203ec1262c97fd794426cefc8904b0debac7340
granis <granis@gmail.com>
2021-05-20 11:42:51 +0200

    add intended structure to app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a full stack mern tik tok clone</h1> 
+      <h1>Lets build a full stack mern tik tok clone</h1>
+
+      {/* app container */}
+      {/* videos */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+
     </div>
   );
 }
