629b580cc47978ef5441efc324f85f2a0f6047d6
granis <granis@gmail.com>
2021-05-25 10:44:34 +0200

    add video css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Video from "./Video";
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a full stack mern tik tok clone</h1>
-
       <div className="app__videos">
         <Video />
         <Video />
--- src/Video.js
+++ src/Video.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./Video.css";
 
 function Video() {
-    return (
-        <div className="video">
-            <video 
-            class="video__player"
-            loop
-            src="http://media.w3.org/2010/05/sintel/trailer.ogv"></video>
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        loop
+        src="http://media.w3.org/2010/05/sintel/trailer.ogv"
+      ></video>
 
-            {/* Videofooter */}
-            {/* Videosidebar */}
-        </div>
-    )
+      {/* Videofooter */}
+      {/* Videosidebar */}
+    </div>
+  );
 }
 
-export default Video
+export default Video;
