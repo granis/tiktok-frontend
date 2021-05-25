69ca30255636c170f82ed2420bef4cca3bcf19d8
granis <granis@gmail.com>
2021-05-25 11:01:08 +0200

    video onclick implementation

--- src/Video.js
+++ src/Video.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
 
 function Video() {
+  const videoRef = useRef(null);
+  const [playing, setPlaying] = useState(false);
+
+  const handleVideoPress = () => {
+    //if video is playing, stop it
+    //if stopped, play it
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+
   return (
     <div className="video">
       <video
+        onClick={handleVideoPress}
         className="video__player"
         loop
+        controls
+        ref={videoRef}
         src="http://media.w3.org/2010/05/sintel/trailer.ogv"
       ></video>
 
